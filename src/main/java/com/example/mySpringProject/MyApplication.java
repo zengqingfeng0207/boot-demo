459156c2af59eb6c8c368050c42c00758875f697
zengqingfeng0207 <zengqingfeng0207@126.com>
2017-12-27 10:33:39 +0800

    main方法启动下，也可以访问jsp操作了

--- src/main/java/com/example/mySpringProject/MyApplication.java
+++ src/main/java/com/example/mySpringProject/MyApplication.java
@@ -1,13 +1,21 @@
 package com.example.mySpringProject;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication 
 public class MyApplication {
 	
-	public static void main(String[] args) {
-		SpringApplication.run(MyApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		Properties properties = new Properties();
+		InputStream is=MyApplication.class.getClassLoader().getResourceAsStream("my-boot-applicatoin.properties");
+		properties.load(is);
+		SpringApplication app = new SpringApplication(MyApplication.class);
+		app.setDefaultProperties(properties);
+		app.run(args);
 	}
 	
 }
